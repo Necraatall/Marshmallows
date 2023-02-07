b0a6cb258421ac2aba0fd1e0cb21d5632653d9b4
Necraatall <seyuki.yamamoro@gmail.com>
2023-02-07 20:17:49 +0100

    update

---
+++ requirements.py
@@ -0,0 +1,15 @@
+-i https://pypi.org/simple
+click==8.1.3 ; python_version >= '3.7'
+flask==2.2.2 ; python_version >= '3.7'
+flask-marshmallow==0.14.0
+flask-sqlalchemy==3.0.3
+greenlet==2.0.2 ; platform_machine == 'aarch64' or (platform_machine == 'ppc64le' or (platform_machine == 'x86_64' or (platform_machine == 'amd64' or (platform_machine == 'AMD64' or (platform_machine == 'win32' or platform_machine == 'WIN32')))))
+itsdangerous==2.1.2 ; python_version >= '3.7'
+jinja2==3.1.2 ; python_version >= '3.7'
+markupsafe==2.1.2 ; python_version >= '3.7'
+marshmallow==3.19.0 ; python_version >= '3.7'
+packaging==23.0 ; python_version >= '3.7'
+six==1.16.0 ; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'
+sqlalchemy==2.0.2 ; python_version >= '3.7'
+typing-extensions==4.4.0 ; python_version >= '3.7'
+werkzeug==2.2.2 ; python_version >= '3.7'
